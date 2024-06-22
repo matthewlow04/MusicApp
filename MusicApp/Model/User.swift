ca8095b2935b89993980ae3e043434c6c50da9f0
Matthew Low <matthew7419@gmail.com>
2024-06-22 17:16:59 -0400

    Initial Commit

---
+++ MusicApp/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  MusicApp
+//
+//  Created by Matthew Low on 2024-06-22.
+//
+
+import Foundation
+
+struct User: Identifiable, Hashable, Codable{
+    
+    let id: String
+    var username: String
+    var profileImageUrl: String
+    var firstName: String
+    var lastName: String?
+    var bio: String?
+    let email: String
+}
---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Matthew Low on 2024-06-22.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
