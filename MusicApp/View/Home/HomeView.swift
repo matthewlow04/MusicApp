469d907a79568377fc5b5ea412e61648f3514ca0
Matthew Low <matthew7419@gmail.com>
2024-08-18 16:36:32 -0400

    Added some navigation links

--- MusicApp/View/Home/HomeView.swift
+++ MusicApp/View/Home/HomeView.swift
@@ -11,141 +11,173 @@ struct HomeView: View {
     var user: User
     var body: some View {
         
-        VStack(alignment:.leading, spacing: 20){
-            Text("Hey, "+user.username+"!")
-                .font(.headline)
-            
-            ScrollView{
-                VStack(alignment:.leading, spacing: 20){
-    //                HStack{
-    //                    Spacer()
-    //                    Text("Music App")
-    //                        .font(.largeTitle)
-    //                    Spacer()
-    //                }
-                    
-                   
-                    
-                    Text("Featured Content")
-                        .modifier(HeaderModifier())
-                        .padding(.bottom, 10)
-                    
-                    ScrollView(.horizontal) {
-                        HStack(spacing: 20) {
-                            ForEach(0..<5) { _ in
-                                VStack {
-                                    AsyncImage(url: URL(string: "https://e-cdn-images.dzcdn.net/images/cover/586eb9f8e98f13c7be5c1835ae1e168d/500x500-000000-80-0-0.jpg")) { image in
-                                        image.resizable()
-                                    } placeholder: {
-                                        Color.gray.opacity(0.5)
+        NavigationStack {
+            VStack(alignment:.leading, spacing: 20){
+                Text("Hey, "+user.username+"!")
+                    .font(.headline)
+                
+                ScrollView{
+                    VStack(alignment:.leading, spacing: 20){
+        //                HStack{
+        //                    Spacer()
+        //                    Text("Music App")
+        //                        .font(.largeTitle)
+        //                    Spacer()
+        //                }
+                        
+                       
+                        
+                        Text("Featured Content")
+                            .modifier(HeaderModifier())
+                            .padding(.bottom, 10)
+                        
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 20) {
+                                ForEach(0..<5) { _ in
+                                    NavigationLink {
+                                        AlbumView()
+                                    } label: {
+                                        VStack {
+                                            AsyncImage(url: URL(string: "https://e-cdn-images.dzcdn.net/images/cover/586eb9f8e98f13c7be5c1835ae1e168d/500x500-000000-80-0-0.jpg")) { image in
+                                                image.resizable()
+                                            } placeholder: {
+                                                Color.gray.opacity(0.5)
+                                            }
+                                            .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.width / 3)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            
+                                            Text("Featured Album")
+                                                .font(.footnote)
+                                                .multilineTextAlignment(.center)
+                                        }
+                                        .foregroundStyle(.foreground)
                                     }
-                                    .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.width / 3)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    
-                                    Text("Featured Album")
-                                        .font(.footnote)
-                                        .multilineTextAlignment(.center)
+
+                                   
                                 }
                             }
-                        }
+                            
+                        }.scrollIndicators(.hidden)
                         
-                    }.scrollIndicators(.hidden)
-                    
-                    Text("Stats")
-                        .modifier(HeaderModifier())
-                    
-                    HStack {
-                        VStack {
-                            Image(systemName: "music.note")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
-                            Text("Songs ")
-                                .multilineTextAlignment(.center)
-                            Text("300")
-                                .multilineTextAlignment(.center)
-                                .bold()
-                        }
-                        .frame(width: UIScreen.main.bounds.width / 6)
+                        Text("Stats")
+                            .modifier(HeaderModifier())
                         
-                        
-                        
-                        Spacer()
-                        VStack {
-                            Image(systemName: "play.rectangle.on.rectangle.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
-                            Text("Albums")
-                                .multilineTextAlignment(.center)
-                            Text("300")
-                                .multilineTextAlignment(.center)
-                                .bold()
-                        }
-                        .frame(width: UIScreen.main.bounds.width / 6)
-                        Spacer()
-                        VStack {
-                            Image(systemName: "person.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
-                            Text("Friends")
-                                .multilineTextAlignment(.center)
-                            Text("300")
-                                .multilineTextAlignment(.center)
-                                .bold()
+                        HStack {
+                            VStack {
+                                Image(systemName: "music.note")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
+                                Text("Songs ")
+                                    .multilineTextAlignment(.center)
+                                Text("300")
+                                    .multilineTextAlignment(.center)
+                                    .bold()
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 6)
+                            
+                            
+                            
+                            Spacer()
+                            VStack {
+                                Image(systemName: "play.rectangle.on.rectangle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
+                                Text("Albums")
+                                    .multilineTextAlignment(.center)
+                                Text("300")
+                                    .multilineTextAlignment(.center)
+                                    .bold()
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 6)
+                            Spacer()
+                            VStack {
+                                Image(systemName: "person.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: UIScreen.screenSize.width / 10, height: UIScreen.screenSize.width / 10)
+                                Text("Friends")
+                                    .multilineTextAlignment(.center)
+                                Text("300")
+                                    .multilineTextAlignment(.center)
+                                    .bold()
+                                
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 6)
                             
                         }
-                        .frame(width: UIScreen.main.bounds.width / 6)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .overlay(RoundedRectangle(cornerRadius: 3 ).stroke(lineWidth: 1)  .foregroundStyle(.gray))
                         
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .overlay(RoundedRectangle(cornerRadius: 3 ).stroke(lineWidth: 1)  .foregroundStyle(.gray))
-                    
-                    VStack(alignment: .leading) {
-                                       HStack {
-                                           Text("Recently Played")
-                                               .font(.headline)
-                                           Spacer()
-                                           Button(action: {
-
-                                           }) {
-                                               Text("View More")
-                                                   .font(.footnote)
-                                                   .foregroundColor(.blue)
-                                           }
-                                       }
-                                       .padding(.bottom, 10)
-                                       
-                                       ForEach(0..<3) { _ in
+                        VStack(alignment: .leading) {
                                            HStack {
-                                               AsyncImage(url: URL(string: "https://e-cdn-images.dzcdn.net/images/cover/586eb9f8e98f13c7be5c1835ae1e168d/500x500-000000-80-0-0.jpg")) { image in
-                                                   image.resizable()
-                                               } placeholder: {
-                                                   Color.gray.opacity(0.5)
-                                               }
-                                               .frame(width: 50, height: 50)
-                                               .clipShape(RoundedRectangle(cornerRadius: 10))
-                                               
-                                               VStack(alignment: .leading) {
-                                                   Text("Song Title")
-                                                       .font(.headline)
-                                                   Text("Artist Name")
-                                                       .font(.subheadline)
-                                               }
+                                               Text("Recently Played")
+                                                   .font(.headline)
                                                Spacer()
+                                               Button(action: {
+
+                                               }) {
+                                                   Text("View More")
+                                                       .font(.footnote)
+                                                       .foregroundColor(.blue)
+                                               }
                                            }
-                                           .padding(.vertical, 5)
+                                           .padding(.bottom, 10)
+                                           
+                                           ForEach(0..<3) { _ in
+                                               
+                                              
+                                               HStack {
+                                                   Button{
+                                                       
+                                                   } label: {
+                                                       AsyncImage(url: URL(string: "https://e-cdn-images.dzcdn.net/images/cover/586eb9f8e98f13c7be5c1835ae1e168d/500x500-000000-80-0-0.jpg")) { image in
+                                                           image.resizable()
+                                                       } placeholder: {
+                                                           Color.gray.opacity(0.5)
+                                                       }
+                                                       .frame(width: 50, height: 50)
+                                                       .clipShape(RoundedRectangle(cornerRadius: 10))
+                                                       
+                                                   }
+                                                   
+                                                  
+                                                   VStack(alignment: .leading) {
+                                                       Button{
+                                                           
+                                                       } label: {
+                                                           Text("Song Title")
+                                                               .font(.headline)
+                                                           
+                                                       }
+                                                       NavigationLink{
+                                                           ArtistView()
+                                                       } label: {
+                                                           Text("Artist Name")
+                                                               .font(.subheadline)
+                                                       }
+                                                       
+                                                      
+                                                   }
+                                                   .foregroundStyle(.foreground)
+                                                   Spacer()
+                                               }
+                                               .padding(.vertical, 5)
+                                           
+
+                                            
+                                           }
                                        }
-                                   }
-                }
-                
-            } .scrollIndicators(.hidden)
+                    }
+                    
+                } .scrollIndicators(.hidden)
+            }
+         
+           
+            .padding()
         }
-     
-       
-        .padding()
         
     }
 }
--- MusicApp/View/SpotifyData/AlbumView.swift
+++ MusicApp/View/SpotifyData/AlbumView.swift
@@ -10,119 +10,128 @@ import SwiftUI
 struct AlbumView: View {
     var body: some View {
         
-        VStack(spacing: 10){
-            AsyncImage(url: URL(string: "https://assets.pippa.io/shows/63613a3a0759420012e3e0a8/1702062526431-dbd0c236a15c3b13bea73817cc870495.jpeg")) { image in
-                image.resizable()
-            } placeholder: {
-                Color.gray.opacity(0.5)
-            }
-            .frame(width: 200, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 5))
-            
-            Text("The Low End Theory")
-                .modifier(TitleModifier())
-            
-            Text("A Tribe Called Quest")
-                .font(.subheadline)
+        NavigationStack {
+            VStack(spacing: 10){
+                AsyncImage(url: URL(string: "https://assets.pippa.io/shows/63613a3a0759420012e3e0a8/1702062526431-dbd0c236a15c3b13bea73817cc870495.jpeg")) { image in
+                    image.resizable()
+                } placeholder: {
+                    Color.gray.opacity(0.5)
+                }
+                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                
+                Text("The Low End Theory")
+                    .modifier(TitleModifier())
+                
+                NavigationLink {
+                    ArtistView()
+                } label: {
+                    Text("A Tribe Called Quest")
+                        .font(.subheadline)
+                        .opacity(0.5)
+                        .foregroundStyle(.foreground)
+                }
+
+               
+                
+                HStack{
+                    Text("14 Tracks")
+                    Text("-")
+                    Text("1991")
+                }
+                .font(.caption)
                 .opacity(0.5)
-            
-            HStack{
-                Text("14 Tracks")
-                Text("-")
-                Text("1991")
-            }
-            .font(.caption)
-            .opacity(0.5)
-           
-            HStack{
-                VStack(alignment: .leading, spacing: 10){
-                    HStack{
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.accentColor)
-                        Text("4.4/5")
+               
+                HStack{
+                    VStack(alignment: .leading, spacing: 10){
+                        HStack{
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.accentColor)
+                            Text("4.4/5")
+                        }
+                        
+                        Text("Average Rating")
                     }
-                    
-                    Text("Average Rating")
+                    Spacer()
+                 
+                    VStack(alignment: .leading, spacing: 10){
+                        HStack{
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                            Text("4.6/5")
+                        }
+                        
+                        Text("Your Rating")
+                    }
+                    .padding(.horizontal)
+                   
                 }
-                Spacer()
+                .font(.subheadline)
+            
+            
+                HStack{
+                    Button(action: {
+
+                    }) {
+                        Text("Rate Album")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10) 
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            )
+                    }
+
+                    Button(action: {
              
-                VStack(alignment: .leading, spacing: 10){
-                    HStack{
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.yellow)
-                        Text("4.6/5")
+                    }) {
+                        Image(systemName: "heart")
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            )
                     }
-                    
-                    Text("Your Rating")
+                   
                 }
-                .padding(.horizontal)
+                .padding(.vertical)
+                
+                Divider()
+                    .padding()
+                
+                
                
-            }
-            .font(.subheadline)
-        
-        
-            HStack{
-                Button(action: {
-
-                }) {
-                    Text("Rate Album")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(10) 
+                
+                HStack{
+                    Text("Song List")
+                    Spacer()
+                    Button("Update"){}
+                        .padding(5)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(Color.blue, lineWidth: 1)
                         )
                 }
-
-                Button(action: {
-         
-                }) {
-                    Image(systemName: "heart")
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.blue, lineWidth: 1)
-                        )
-                }
-               
+              
+                
+                ScrollView{
+                    VStack(spacing:5){
+                        AlbumSongView(number: 1, rating: 0.4)
+                        AlbumSongView(number: 2, rating: 0.4)
+                        AlbumSongView(number: 3, rating: 0.4)
+                        AlbumSongView(number: 4, rating: 0.4)
+                        AlbumSongView(number: 5, rating: 0.4)
+                    }
+                }.scrollIndicators(.hidden)
+                
             }
-            .padding(.vertical)
-            
-            Divider()
-                .padding()
-            
-            
-           
-            
-            HStack{
-                Text("Song List")
-                Spacer()
-                Button("Update"){}
-                    .padding(5)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 1)
-                    )
-            }
-          
-            
-            ScrollView{
-                VStack(spacing:5){
-                    AlbumSongView(number: 1, rating: 0.4)
-                    AlbumSongView(number: 2, rating: 0.4)
-                    AlbumSongView(number: 3, rating: 0.4)
-                    AlbumSongView(number: 4, rating: 0.4)
-                    AlbumSongView(number: 5, rating: 0.4)
-                }
-            }.scrollIndicators(.hidden)
-            
-        }
-        .padding()
+            .padding()
         .fontDesign(.rounded)
+        }
         
     }
 }
